80e2df80854d482fda7675858354c4ba9785217a
Xitonight <xitonight@gmail.com>
2025-11-05 13:29:16 +0100

    feat(zen): use user.js instead of prefs.js
    Also added search.json.mozlz4 that contains the config for search
    engines

---
+++ dots/.zen/xitonight/user.js
@@ -0,0 +1,11 @@
+// Fonts
+user_pref("font.name.monospace.x-western", "monospace");
+user_pref("font.name.sans-serif.x-western", "sans-serif");
+user_pref("font.name.serif.x-western", "serif");
+user_pref("font.size.monospace.x-western", 13);
+user_pref("font.size.variable.x-western", 13);
+
+// Ui related
+user_pref("zen.tabs.show-newtab-vertical", false);
+user_pref("zen.tabs.vertical.right-side", true);
+user_pref("zen.view.compact.toolbar-flash-popup", true);
