d9146f74b195fd3cac9b900172d2f3abffed8d4e
Xitonight <xitonight@gmail.com>
2025-12-11 01:56:22 +0100

    feat(nvim): add codesnap plugin
    Also restored bottom search bar in noice.nvim

--- dots/.config/nvim/lua/plugins/init.lua
+++ dots/.config/nvim/lua/plugins/init.lua
@@ -141,7 +141,11 @@ return {
   },
 
   -- Fast code snippets
-  { "mistricky/codesnap.nvim", tag = "v2.0.0-beta.17" },
+  {
+    "mistricky/codesnap.nvim",
+    tag = "v2.0.0-beta.17",
+    lazy = false,
+  },
 
   -- Zen mode (remove number column, disable tmux statusline, increase font)
   {
@@ -219,7 +223,7 @@ return {
       },
       -- you can enable a preset for easier configuration
       presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
+        bottom_search = true, -- use a classic bottom cmdline for search
         command_palette = false, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
@@ -229,10 +233,6 @@ return {
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      -- "rcarriga/nvim-notify",
     },
   },
 }
