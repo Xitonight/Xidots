c423242e0f36a4c8c2b4c8a46c5225ca7758a728
Xitonight <xitonight@gmail.com>
2026-05-11 08:37:37 +0200

    refactor(nvim): clear init.lua
    - Removed autocmd to run fix imports on golang files
    - Removed Noice hl group linking (moved to another file)

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -106,33 +106,3 @@ autocmd("BufReadPost", {
 vim.filetype.add {
   pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
 }
-
-autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    local params = vim.lsp.util.make_range_params(0, "utf-8")
-    params.context = { only = { "source.organizeImports" } }
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-    for cid, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-          vim.lsp.util.apply_workspace_edit(r.edit, enc)
-        end
-      end
-    end
-    vim.lsp.buf.format { async = false }
-  end,
-})
-
--- Match Noice Signature/Doc colors to CMP/Standard Floats
-local highlights = {
-  -- The background of the signature help window
-  NoicePopup = { link = "CmpPmenu" },
-  -- The border of the signature help window
-  NoicePopupBorder = { link = "CmpPmenuBorder" },
-}
-
-for group, settings in pairs(highlights) do
-  vim.api.nvim_set_hl(0, group, settings)
-end
