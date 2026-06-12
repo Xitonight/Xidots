c0fbede0b46e9dff3678d4d86b244d8221873a15
Xitonight <xitonight@gmail.com>
2026-06-12 20:54:27 +0200

    feat(rofi): add wallpaper script/menu

--- dots/.config/hypr/source/keybinds.lua
+++ dots/.config/hypr/source/keybinds.lua
@@ -135,6 +135,7 @@ hl.bind("SUPER + ALT + K", hl.dsp.exec_cmd("hyprctl switchxkblayout current next
 hl.bind("SUPER + A", hl.dsp.exec_cmd("pkill rofi || rofi -show drun"))
 hl.bind("SUPER + TAB", hl.dsp.exec_cmd("pkill rofi || rofi -show window -config ~/.config/rofi/window.rasi"))
 hl.bind("SUPER + Period", hl.dsp.exec_cmd("pkill rofi || rofi -show emoji -config ~/.config/rofi/emoji.rasi"))
+hl.bind("SUPER + Comma", hl.dsp.exec_cmd("pkill rofi || ~/.local/bin/rofi-wallpaper"))
 hl.bind("SUPER + V", hl.dsp.exec_cmd("pkill rofi || ~/.local/bin/rofi-clipboard"))
 
 -- System
