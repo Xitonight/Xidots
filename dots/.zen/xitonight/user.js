1cc1a2af46a3499d05b029a595b5bb659aad09f5
Xitonight <xitonight@gmail.com>
2026-06-14 22:06:32 +0200

    feat(zen): update keybinds and make font size consistent

--- dots/.zen/xitonight/user.js
+++ dots/.zen/xitonight/user.js
@@ -2,8 +2,8 @@
 user_pref("font.name.monospace.x-western", "monospace");
 user_pref("font.name.sans-serif.x-western", "sans-serif");
 user_pref("font.name.serif.x-western", "serif");
-user_pref("font.size.monospace.x-western", 13);
-user_pref("font.size.variable.x-western", 13);
+user_pref("font.size.monospace.x-western", 14);
+user_pref("font.size.variable.x-western", 14);
 
 // Ui related
 user_pref("zen.tabs.show-newtab-vertical", false);
