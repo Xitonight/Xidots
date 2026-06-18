fbcd21a227a7698e5c480ba57944d8ca3947c79e
Xitonight <xitonight@gmail.com>
2026-06-19 00:07:19 +0200

    feat(hypr): improve animations for layers

--- dots/.config/hypr/source/animations.lua
+++ dots/.config/hypr/source/animations.lua
@@ -5,6 +5,7 @@ hl.config({
 })
 
 hl.curve("myBezier", { type = "bezier", points = { { 0.4, 0.0 }, { 0.2, 1.0 } } })
+hl.curve("mySpring", { type = "spring", mass = 1, stiffness = 70, dampening = 13 })
 
 hl.animation({ leaf = "windows", enabled = true, speed = 2.5, bezier = "myBezier", style = "popin 80%" })
 hl.animation({ leaf = "border", enabled = true, speed = 2.5, bezier = "myBezier" })
@@ -16,3 +17,5 @@ hl.animation({
 	bezier = "myBezier",
 	style = "slidefadevert 20%",
 })
+
+hl.animation({ leaf = "layers", enabled = true, speed = 1, spring = "mySpring", style = "slide bottom" })
